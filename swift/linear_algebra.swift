7d80874a5be5f3106c67f0e4fa2259181e9bd4d5
Michael Mellinger <mmellinger66@gmail.com>
2020-01-27 21:44:27 -0500

    Update linear_algebra.swift

--- swift/linear_algebra.swift
+++ swift/linear_algebra.swift
@@ -1,7 +1,60 @@
 typealias Vector = [Float]
 typealias Matrix = [[Float]]
 
-let A = [[1, 2, 3],
+// pg 56
+
+func add(_ v: Vector, _ w: Vector) -> Vector {
+    var result:Vector = []
+    assert(v.count == w.count, "vectors must be the same length")
+    for (v_i, w_i) in zip(v, w) {
+        let x = v_i + w_i
+        result.append(x)
+    }
+    return result
+}
+
+func subtract(_ v: Vector, _ w: Vector) -> Vector {
+    var result:Vector = []
+    assert(v.count == w.count, "vectors must be the same length")
+    for (v_i, w_i) in zip(v, w) {
+        let x = v_i - w_i
+        result.append(x)
+    }
+    return result
+}
+
+assert(add([1, 2, 3], [4, 5, 6]) == [5, 7, 9])
+assert(subtract([5, 7, 9], [4, 5, 6]) == [1, 2, 3])
+
+
+func vector_sum(_ vectors: [Vector]) -> Vector {
+    assert(vectors.count > 0, "no vectors provided")
+    let vectorLength = vectors.first?.count ?? 0
+    //let fiveZs = Array(repeating: "Z", count: vectorLength)
+    var result:Vector = Array(repeating: 0, count: vectorLength)
+    //assert(v.count == w.count, "vectors must be the same length")
+    for vec in vectors {
+        for i in 0..<vectorLength {
+            result[i] += vec[i]
+        }
+        
+    }
+    return result
+}
+
+assert(vector_sum([[1,2],[3,4],[5,6],[7,8]]) == [16,20])
+
+
+func scalar_multiply(_ c:Float, _ v:Vector) -> Vector {
+     v.map {$0 * c}
+}
+
+
+assert(scalar_multiply(2, [1, 2, 3]) == [2, 4, 6])
+
+// pg 60
+
+let A:Matrix = [[1, 2, 3],
          [4, 5, 6]]
 
 

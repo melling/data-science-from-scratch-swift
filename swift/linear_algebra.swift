b98f81f298b78f84d2563fa46c8896274ba5406f
Michael Mellinger <mmellinger66@gmail.com>
2020-02-27 09:26:09 -0500

    Update linear_algebra.swift

--- swift/linear_algebra.swift
+++ swift/linear_algebra.swift
@@ -105,8 +105,146 @@ func distance(_ v:Vector, _ w:Vector) -> Float {magnitude(subtract(v, w))}
 
 // pg 60
 
+
+
+/*
+ Data Science from Scratch
+ Joel Grus
+ Chapter 4: Linear Algebra
+ Implemented in Swift 5.x
+ 
+ Python: https://github.com/joelgrus/data-science-from-scratch/blob/master/scratch/linear_algebra.py
+ 
+ - Sticking with snake_case variable names for consistency
+ 
+ */
+
+import Foundation //sqrt
+
+public typealias Vector = [Float]
+public typealias Matrix = [[Float]]
+
+// pg 56
+
+public func add(_ v: Vector, _ w: Vector) -> Vector {
+    var result:Vector = []
+    assert(v.count == w.count, "vectors must be the same length")
+    for (v_i, w_i) in zip(v, w) {
+        let x = v_i + w_i
+        result.append(x)
+    }
+    return result
+}
+
+public func subtract(_ v: Vector, _ w: Vector) -> Vector {
+    var result:Vector = []
+    assert(v.count == w.count, "vectors must be the same length")
+    for (v_i, w_i) in zip(v, w) {
+        let x = v_i - w_i
+        result.append(x)
+    }
+    return result
+}
+
+func test_add_subtract() {
+    assert(add([1, 2, 3], [4, 5, 6]) == [5, 7, 9])
+    assert(subtract([5, 7, 9], [4, 5, 6]) == [1, 2, 3])
+}
+
+let test0 = test_add_subtract()
+
+
+
+public func vector_sum(_ vectors: [Vector]) -> Vector {
+    assert(vectors.count > 0, "no vectors provided")
+    let vectorLength = vectors.first?.count ?? 0
+    
+    var result:Vector = Array(repeating: 0, count: vectorLength)
+    //assert(v.count == w.count, "vectors must be the same length")
+    for vec in vectors {
+        for i in 0..<vectorLength {
+            result[i] += vec[i]
+        }
+        
+    }
+    return result
+}
+
+
+func test_vector_sum() {
+    assert(vector_sum([[1,2],[3,4],[5,6],[7,8]]) == [16,20])
+}
+
+let test2 = test_vector_sum()
+
+
+public func scalar_multiply(_ c:Float, _ v:Vector) -> Vector {
+    v.map {$0 * c}
+}
+
+func test_scalar_multiply() {
+    assert(scalar_multiply(2, [1, 2, 3]) == [2, 4, 6])
+}
+
+//
+
+public func vector_mean(_ vectors:[Vector]) -> Vector {
+    let n:Float = Float(vectors.count)
+    return scalar_multiply(Float(1/n), vector_sum(vectors))
+}
+
+func test_vector_mean() {
+    vector_mean([[1,2], [3,4], [5,6]])
+    
+    assert(vector_mean([[1,2], [3,4], [5,6]]) == [3,4])
+}
+
+
+
+// pg 58
+
+public func dot(_ v:Vector, _ w:Vector) -> Float {
+    assert(v.count == w.count, "vectors must be the same length")
+    var total:Float = 0
+    for (v_i, w_i) in zip(v, w) {
+        total += v_i * w_i
+    }
+    return total
+}
+func test_dot() {
+    assert(dot([1, 2, 3], [4, 5, 6]) == 32)
+}
+
+//
+
+public func sum_of_squares(_ v:Vector) -> Float {dot(v, v)}
+
+func test_sum_of_squares() {
+    assert(sum_of_squares([1, 2, 3]) == 14) // 1*1 + 2*2 + 3*3
+}
+
+//
+
+public func magnitude(_ v:Vector) -> Float {sqrt(sum_of_squares(v))}
+
+func test_magnitude() {
+    assert(magnitude([3, 4]) == 5)
+}
+
+//
+
+// pg 59
+
+public func squared_distance(_ v:Vector, _ w:Vector) -> Float {sum_of_squares(subtract(v, w))}
+
+public func distance(_ v:Vector, _ w:Vector) -> Float {magnitude(subtract(v, w))}
+
+
+
+// pg 60
+
 let A:Matrix = [[1, 2, 3],
-         [4, 5, 6]]
+                [4, 5, 6]]
 
 
 let B = [[1, 2],
@@ -115,7 +253,7 @@ let B = [[1, 2],
 
 // p60
 
-func shape(A:Matrix) -> (Int, Int) {
+public func shape(A:Matrix) -> (Int, Int) {
     let num_rows = A.count
     let num_cols = A[0].count
     
@@ -124,10 +262,10 @@ func shape(A:Matrix) -> (Int, Int) {
 
 // p60
 
-func get_row(A: Matrix, i: Int) -> Vector { A[i] }
+public func get_row(A: Matrix, i: Int) -> Vector { A[i] }
 
 
-func get_column(A: Matrix, j: Int) -> Vector {
+public func get_column(A: Matrix, j: Int) -> Vector {
     var vec:Vector = []
     
     for A_i in A {
@@ -137,10 +275,10 @@ func get_column(A: Matrix, j: Int) -> Vector {
     return vec
 }
 
-func make_matrix(num_rows: Int,
+public func make_matrix(num_rows: Int,
                  num_cols: Int
-                 //entry_fn
-                 ) -> Matrix {
+    //entry_fn
+) -> Matrix {
     var m:Matrix = [[]]
     
     return m
@@ -148,7 +286,7 @@ func make_matrix(num_rows: Int,
 
 //  p61
 
-func identity_matrix(_ n:Int) -> Matrix {
+public func identity_matrix(_ n:Int) -> Matrix {
     var m:Matrix = []
     for i in 0..<n  {
         var row:[Float] = []
@@ -161,7 +299,10 @@ func identity_matrix(_ n:Int) -> Matrix {
     return m
 }
 
-assert(shape(A: [[1, 2, 3], [4, 5, 6]]) == (2, 3))
+func test_shape() {
+    assert(shape(A: [[1, 2, 3], [4, 5, 6]]) == (2, 3))
+}
+
+//
 
 let identity = identity_matrix(3)
-

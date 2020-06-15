3229372c2b690e74e3fb88dd65a79596571f2baf
Michael Mellinger <mmellinger66@gmail.com>
2020-06-15 14:34:05 -0400

    Update probability.swift
    Use Double instead of Float

--- swift/probability.swift
+++ swift/probability.swift
@@ -1,3 +1,4 @@
+
 /*
  Chapter 6: Probability
  
@@ -8,46 +9,49 @@ import Foundation //sqrt, erf
 
 // 77
 
-enum Kid {
+private enum Kid {
     case boy
     case girl
 }
 
-func random_kid() -> Kid {
+private func random_kid() -> Kid {
     Bool.random() ? .boy : .girl
 }
-
-var both_girls = 0
-var older_girl = 0
-var either_girl = 0
-
-for _ in 1 ... 10000 {
-    let younger = random_kid()
-    let older = random_kid()
+func run0() {
+    var both_girls = 0
+    var older_girl = 0
+    var either_girl = 0
     
-    if older == .girl {
-        older_girl += 1
+    for _ in 1 ... 10000 {
+        let younger = random_kid()
+        let older = random_kid()
+        
+        if older == .girl {
+            older_girl += 1
+        }
+        if older == .girl && younger == .girl {
+            both_girls += 1
+        }
+        if older == .girl || younger == .girl {
+            either_girl += 1
+        }
     }
-    if older == .girl && younger == .girl {
-        both_girls += 1
-    }
-    if older == .girl || younger == .girl {
-        either_girl += 1
-    }
+    
+    let both_older = Double(both_girls) / Double(older_girl)
+    let both_either = Double(both_girls) / Double(either_girl)
+    
+    print("P(both | older): \(both_older)")   // 0.514 ~ 1/2
+    print("P(both | either): \(both_either)") // 0.342 ~ 1/3
+    
 }
 
-let both_older = Float(both_girls) / Float(older_girl)
-let both_either = Float(both_girls) / Float(either_girl)
-
-print("P(both | older): \(both_older)")   // 0.514 ~ 1/2
-print("P(both | either): \(both_either)") // 0.342 ~ 1/3
 
 // Continuous Distributions - p80 
-func uniform_pdf(_ x:Float) -> Float {
+public func uniform_pdf(_ x:Double) -> Double {
     x >= 0 && x < 1 ? 1 : 0
 }
 
-func uniform_cdf(_ x:Float) -> Float {
+public func uniform_cdf(_ x:Double) -> Double {
     switch x {
     case _ where x < 0: return 0
     case _ where x < 1: return x
@@ -59,8 +63,8 @@ func uniform_cdf(_ x:Float) -> Float {
 
 // Normal Distributions - p81
 
-func normal_pdf(x:Float, mu:Float = 0, sigma:Float = 1) -> Float {
-    let sqrt_two_pi = sqrt(2 * Float.pi)
+public func normal_pdf(x:Double, mu:Double = 0, sigma:Double = 1) -> Double {
+    let sqrt_two_pi = sqrt(2 * Double.pi)
     let a = (x - mu)
     let numerator = pow(a, 2)
     let denominator = 2 * pow(sigma, 2)
@@ -79,19 +83,19 @@ func normal_pdf(x:Float, mu:Float = 0, sigma:Float = 1) -> Float {
  
  p83
  */
-func normal_cdf(x:Float, mu:Float = 0, sigma:Float = 1) -> Float {
-     (1 + erf((x - mu) / sqrt(2) / sigma)) / 2
+public func normal_cdf(x:Double, mu:Double = 0, sigma:Double = 1) -> Double {
+    (1 + erf((x - mu) / sqrt(2) / sigma)) / 2
 }
 
-func inverse_normal_cdf(p:Float, mu:Float = 0, sigma:Float = 1, tolerance:Float = 0.00001) -> Float {
+public func inverse_normal_cdf(p:Double, mu:Double = 0, sigma:Double = 1, tolerance:Double = 0.00001) -> Double {
     if mu != 0 || sigma != 1 {
         return  mu + sigma + inverse_normal_cdf(p: p, tolerance: tolerance)
     }
-    var low_z:Float = -10.0
-    var hi_z:Float = 10.0
-    var mid_z:Float = 0
+    var low_z:Double = -10.0
+    var hi_z:Double = 10.0
+    var mid_z:Double = 0
     while hi_z - low_z > tolerance {
-         mid_z = (low_z + hi_z) / 2
+        mid_z = (low_z + hi_z) / 2
         let mid_p = normal_cdf(x: mid_z)
         if mid_p < p {
             low_z = mid_z
@@ -108,11 +112,12 @@ func inverse_normal_cdf(p:Float, mu:Float = 0, sigma:Float = 1, tolerance:Float 
  p84
  */
 
-func bernoulli_trial(_ p:Float) -> Int {
-    Float.random(in: 0...1) < p ? 1 : 0
+public func bernoulli_trial(_ p:Double) -> Int {
+    Double.random(in: 0...1) < p ? 1 : 0
 }
 
-func binomial(n:Int, p:Float) -> Int {
+public func binomial(n:Int, p:Double) -> Int {
     if n == 0 {return 0}
     return bernoulli_trial(p) + binomial(n: n-1, p: p)
 }
+

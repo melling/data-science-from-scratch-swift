4ec94bd449255ed366c38b636e5de71aa68f1719
Michael Mellinger <mmellinger66@gmail.com>
2020-06-15 15:50:50 -0400

    Update gradient_descent.swift
    Use Double instead of Float

--- swift/gradient_descent.swift
+++ swift/gradient_descent.swift
@@ -1,128 +1,53 @@
 
+
 /*
  Chapter 8: Gradient Descent
  
- https://github.com/joelgrus/data-science-from-scratch/blob/master/scratch/gradient_descent.py
+ Python: https://github.com/joelgrus/data-science-from-scratch/blob/master/scratch/gradient_descent.py
  
  */
 
 import Foundation //sqrt
 
-/*
- ## Linear Algebra imports:
- - add()
- - distance()
- - dot()
- - scalar_multiply()
- - vector_mean()
- 
- ### Indirect
- - subtract()
- - sum_of_squares()
- - magnitude()
- - vector_sum()
- 
- */
-typealias Vector = [Float]
-
-func add(_ v: Vector, _ w: Vector) -> Vector {
-    var result:Vector = []
-    assert(v.count == w.count, "vectors must be the same length")
-    for (v_i, w_i) in zip(v, w) {
-        let x = v_i + w_i
-        result.append(x)
-    }
-    return result
-}
-
-func subtract(_ v: Vector, _ w: Vector) -> Vector {
-    var result:Vector = []
-    assert(v.count == w.count, "vectors must be the same length")
-    for (v_i, w_i) in zip(v, w) {
-        let x = v_i - w_i
-        result.append(x)
-    }
-    return result
-}
-
-func dot(_ v:Vector, _ w:Vector) -> Float {
-    assert(v.count == w.count, "vectors must be the same length")
-    var total:Float = 0
-    for (v_i, w_i) in zip(v, w) {
-        total += v_i * w_i
-    }
-    return total
-}
-
-func vector_sum(_ vectors: [Vector]) -> Vector {
-    assert(vectors.count > 0, "no vectors provided")
-    let vectorLength = vectors.first?.count ?? 0
-    
-    var result:Vector = Array(repeating: 0, count: vectorLength)
-    //assert(v.count == w.count, "vectors must be the same length")
-    for vec in vectors {
-        for i in 0..<vectorLength {
-            result[i] += vec[i]
-        }
-        
-    }
-    return result
-}
-
-
-func scalar_multiply(_ c:Float, _ v:Vector) -> Vector {v.map {$0 * c}}
-
-func vector_mean(_ vectors:[Vector]) -> Vector {
-    let n:Float = Float(vectors.count)
-    return scalar_multiply(Float(1/n), vector_sum(vectors))
-}
-
-func sum_of_squares(_ v:Vector) -> Float {dot(v, v)}
-func magnitude(_ v:Vector) -> Float {sqrt(sum_of_squares(v))}
-func distance(_ v:Vector, _ w:Vector) -> Float {magnitude(subtract(v, w))}
-
-/*
- 
- Chapter 8
- */
 
 // p101
 
-func sum_of_squares(v:Vector) -> Float {
+func sum_of_squares(v:Vector) -> Double {
     dot(v, v)
 }
 
-sum_of_squares(v: [2,2])
+//sum_of_squares(v: [2,2])
 
-func difference_quotient(f:(Float) -> Float, x:Float, h:Float) -> Float {
+func difference_quotient(f:(Double) -> Double, x:Double, h:Double) -> Double {
     let r = (f(x + h) - f(x)) / h
     return r
 }
 
 
-func square(_ x:Float) -> Float { x * x }
+func square(_ x:Double) -> Double { x * x }
 
-func derivative(_ x:Float) -> Float { 2 * x }
+func derivative(_ x:Double) -> Double { 2 * x }
 
-func test1() {
+private func test_derivative() {
     let xs = (-10..<11)
-    let actuals = xs.map {derivative(Float($0))}
-    let estimates = xs.map {difference_quotient(f: square(_:), x:Float($0), h: 0.001)}
+    let actuals = xs.map {derivative(Double($0))}
+    let estimates = xs.map {difference_quotient(f: square(_:), x:Double($0), h: 0.001)}
 }
 
-test1()
+private let test1 = test_derivative()
+
 
 // p104
 
-func partial_difference_quotient(f:(Vector) -> Float,
+private func partial_difference_quotient(f:(Vector) -> Double,
                                  v:Vector,
                                  i:Int,
-                                 h:Float) -> Float {
+                                 h:Double) -> Double {
     //let r = (f(x + h) - f(x)) / h
     var w:Vector = []
     
     for (j, v_j) in v.enumerated() {
-        let a:Float
+        let a:Double
         if j == i {
             a = v_j + h
         } else {
@@ -136,9 +61,9 @@ func partial_difference_quotient(f:(Vector) -> Float,
 
 // p105
 
-func estimate_gradient(f:(Vector) -> Float,
+private func estimate_gradient(f:(Vector) -> Double,
                        v:Vector,
-                       h:Float = 0.0001) -> Vector {
+                       h:Double = 0.0001) -> Vector {
     var vec:Vector = []
     
     for i in 0 ..< v.count {
@@ -148,7 +73,7 @@ func estimate_gradient(f:(Vector) -> Float,
     return vec
 }
 
-func gradient_step(v:Vector, gradient:Vector, step_size:Float) -> Vector {
+public func gradient_step(v:Vector, gradient:Vector, step_size:Double) -> Vector {
     assert(v.count == gradient.count)
     let step = scalar_multiply(step_size, gradient)
     return add(v, step)
@@ -158,8 +83,8 @@ func sum_of_squares_gradient(_ v:Vector) -> Vector {
     v.map {2 * $0}
 }
 
-func use_gradient() {
-    var v:Vector = (0..<3).map {_ in Float.random(in: -10...10)}
+private func use_gradient() {
+    var v:Vector = (0..<3).map {_ in Double.random(in: -10...10)}
     
     for epoch in 0 ..< 1000 {
         let grad = sum_of_squares_gradient(v)
@@ -173,7 +98,7 @@ func use_gradient() {
 
 //use_gradient()
 
-func linear_gradient(x:Float, y:Float, theta:Vector) -> Vector {
+func linear_gradient(x:Double, y:Double, theta:Vector) -> Vector {
     let slope = theta[0]
     let intercept = theta[1]
     
@@ -188,14 +113,14 @@ func linear_gradient(x:Float, y:Float, theta:Vector) -> Vector {
  
  p107
  */
-func use_gradient2(learning_rate:Float = 0.001) {
+private func use_gradient2(learning_rate:Double = 0.001) {
     let inputs:[(Int,Int)] = (-50...49).map {($0, 20 * $0 + 5)}
-    var theta:Vector = [Float.random(in: -1...1), Float.random(in: -1...1)]
+    var theta:Vector = [Double.random(in: -1...1), Double.random(in: -1...1)]
     //let learning_rate:Float = 0.001
     
     for epoch in 0..<5000 {
         for (x, y) in inputs {
-            let lgrads:Vector = linear_gradient(x: Float(x), y: Float(y), theta: theta)
+            let lgrads:Vector = linear_gradient(x: Double(x), y: Double(y), theta: theta)
             let grad = vector_mean([lgrads])
             theta = gradient_step(v: theta, gradient: grad, step_size: -learning_rate)
             print("\(epoch), \(theta)")
@@ -225,14 +150,14 @@ func minibatches<T>(dataset:[T], batch_size:Int, shuffle:Bool = true) -> [T] {
     return dataset
 }
 
-func test_minibatch(learning_rate:Float = 0.001) {
-    var theta:Vector = [Float.random(in: -1...1), Float.random(in: -1...1)]
+private func test_minibatch(learning_rate:Double = 0.001) {
+    var theta:Vector = [Double.random(in: -1...1), Double.random(in: -1...1)]
     let inputs:[(Int,Int)] = (-50...49).map {($0, 20 * $0 + 5)}
     
     for epoch in 0..<1000 {
         let batches = minibatches(dataset: inputs, batch_size: 20)
         for (x, y) in inputs {
-            let lgrads:Vector = linear_gradient(x: Float(x), y: Float(y), theta: theta)
+            let lgrads:Vector = linear_gradient(x: Double(x), y: Double(y), theta: theta)
             let grad = vector_mean([lgrads])
             theta = gradient_step(v: theta, gradient: grad, step_size: -learning_rate)
             print("\(epoch), \(theta)")
@@ -242,13 +167,13 @@ func test_minibatch(learning_rate:Float = 0.001) {
 
 // p109
 
-func test_stochastic_gradient_descent(learning_rate:Float = 0.001) {
-    var theta:Vector = [Float.random(in: -1...1), Float.random(in: -1...1)]
+private func test_stochastic_gradient_descent(learning_rate:Double = 0.001) {
+    var theta:Vector = [Double.random(in: -1...1), Double.random(in: -1...1)]
     let inputs:[(Int,Int)] = (-50...49).map {($0, 20 * $0 + 5)}
     
     for epoch in 0..<100 {
         for (x, y) in inputs {
-            let lgrads:Vector = linear_gradient(x: Float(x), y: Float(y), theta: theta)
+            let lgrads:Vector = linear_gradient(x: Double(x), y: Double(y), theta: theta)
             let grad = vector_mean([lgrads])
             theta = gradient_step(v: theta, gradient: grad, step_size: -learning_rate)
             print("\(epoch), \(theta)")
